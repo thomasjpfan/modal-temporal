6104609bc23b01d994c56af6ba68dcc06898bb4a
Thomas J. Fan <thomasjpfan@gmail.com>
2026-05-17 20:02:37 -0400

    Add a cls based worker

--- modal_worker.py
+++ modal_worker.py
@@ -67,6 +67,29 @@ async def add_two(value: int) -> int:
 add_two_runner = app.function(env=env, image=image)(modal_activity(add_two))
 
 
+@activity.defn
+class SayHello:
+    def __init__(self, greeting: str = "Hello again"):
+        self.greeting = greeting
+
+    async def run(self, name: str) -> str:
+        return f"{self.greeting}, {name}!"
+
+
+@app.cls()
+class SayHelloRunner:
+    greeting: str = modal.parameter()
+
+    @modal.enter()
+    async def start(self):
+        self.worker_obj = SayHello(greeting=self.greeting)
+
+    @modal.method()
+    async def run(self, task_token: bytes, args: Any) -> None:
+        client = await get_temporal_client()
+        return await run_activity(self.worker_obj.run, args, client, task_token)
+
+
 @workflow.defn
 class SayHelloWorkflow:
     @workflow.run
@@ -74,8 +97,11 @@ class SayHelloWorkflow:
         result = await workflow.execute_activity(
             greet, name, schedule_to_close_timeout=timedelta(seconds=30)
         )
+        more_result = await workflow.execute_activity_method(
+            SayHello.run, result, schedule_to_close_timeout=timedelta(seconds=30)
+        )
         count = await workflow.execute_activity(
-            word_count, result, schedule_to_close_timeout=timedelta(seconds=30)
+            word_count, more_result, schedule_to_close_timeout=timedelta(seconds=30)
         )
         return await workflow.execute_activity(
             add_two, count, schedule_to_close_timeout=timedelta(seconds=30)
@@ -91,14 +117,15 @@ async def queuer():
     An alternative is to run this queuer on a machine external to Modal.
     """
     client = await get_temporal_client()
+    greeting_activity = SayHello(greeting="You are great")
     worker = Worker(
         client,
         task_queue="my-task-queue",
         workflows=[SayHelloWorkflow],
-        activities=[greet, word_count, add_two],
+        activities=[greet, word_count, add_two, greeting_activity],
         interceptors=[DispatchInterceptor(APP_NAME)],
         # Add a thread pool executor so we can run sync activities
-        activity_executor=ThreadPoolExecutor(max_workers=1),
+        activity_executor=ThreadPoolExecutor(max_workers=4),
     )
     print("Dispatcher worker started. Activities will be completed by a Modal function")
     await worker.run()
